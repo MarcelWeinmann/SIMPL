bc96c5dd69045f78d1ebfa6ce26a81a0d0001231
Marcel Weinmann <marcel.weinmann@tum.de>
2025-02-23 19:39:00 +0100

    update training configs

--- config/simpl_av2_cfg.py
+++ config/simpl_av2_cfg.py
@@ -19,7 +19,7 @@ class AdvCfg():
     def get_net_cfg(self):
         net_cfg = dict()
         net_cfg["network"] = "simpl.simpl:Simpl"
-        net_cfg["init_weights"] = False
+        net_cfg["init_weights"] = True
         net_cfg["in_actor"] = 14
         net_cfg["d_actor"] = 128
         net_cfg["n_fpn_scale"] = 4
@@ -43,8 +43,8 @@ class AdvCfg():
     def get_loss_cfg(self):
         loss_cfg = dict()
         loss_cfg["loss_fn"] = "simpl.av2_loss_fn:LossFunc"
-        loss_cfg["cls_coef"] = 0.1
-        loss_cfg["reg_coef"] = 0.9
+        loss_cfg["cls_coef"] = 0.0
+        loss_cfg["reg_coef"] = 1.0
         loss_cfg["mgn"] = 0.2
         loss_cfg["cls_th"] = 2.0
         loss_cfg["cls_ignore"] = 0.2
@@ -77,8 +77,8 @@ class AdvCfg():
             opt_cfg['gamma'] = 0.1
         elif opt_cfg['scheduler'] == 'polyline':
             opt_cfg['init_lr'] = 1e-4
-            opt_cfg['milestones'] = [0, 5, 35, 40]
-            opt_cfg['values'] = [1e-4, 1e-3, 1e-3, 1e-4]
+            opt_cfg['milestones'] = [0, 20, 150, 300, 400]
+            opt_cfg['values'] = [1e-4, 1e-3, 1e-3, 1e-4, 1e-5]
 
         opt_cfg.update(self.g_cfg)  # append global config
         return opt_cfg
@@ -87,7 +87,7 @@ class AdvCfg():
         eval_cfg = dict()
         eval_cfg['evaluator'] = 'utils.evaluator:TrajPredictionEvaluator'
         eval_cfg['data_ver'] = 'av2'
-        eval_cfg['miss_thres'] = 2.0
+        eval_cfg['miss_thres'] = 5.0
 
         eval_cfg.update(self.g_cfg)  # append global config
         return eval_cfg
